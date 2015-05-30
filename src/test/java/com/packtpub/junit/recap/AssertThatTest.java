ec59ac9920c8141c59816a0d32cae723bcdadfaf
lazaromedina <lazaro.medina@gmail.com>
2015-05-30 20:02:00 +0200

    added custom matcher

--- src/test/java/com/packtpub/junit/recap/AssertThatTest.java
+++ src/test/java/com/packtpub/junit/recap/AssertThatTest.java
@@ -7,7 +7,18 @@ import static org.hamcrest.CoreMatchers.either;
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.both;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.containsString;
+
+//import static com.packtpub.junit.recap.LessThanOrEqual;
+
 import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -36,6 +47,33 @@ public class AssertThatTest {
         assertThat(marks, anyOf(is(100.00), is(1.00), is(55.00),is(88.00), is(67.8)));
         assertThat(marks, not(anyOf(is(0.00), is(200.00))));
         assertThat(marks, not(allOf(is(1.00), is(100.00), is(30.00))));
+    }
+
+    @Test
+    public void verify_collection_values() throws Exception {
+        List<Double> salary = Arrays.asList(50.0, 200.0, 500.0);
+        assertThat(salary, hasItem(50.00));
+        assertThat(salary, hasItems(50.00, 200.00));
+        assertThat(salary, not(hasItem(1.00)));
+    }
+
+    @Test
+    public void verify_Strings() throws Exception {
+        String name = "John Jr Dale";
+        assertThat(name, startsWith("John"));
+        assertThat(name, endsWith("Dale"));
+        assertThat(name, containsString("Jr"));
+    }
+
+    @Test
+    public void lessthanOrEquals_custom_matcher() throws Exception {
+        int actualGoalScored = 2;
+        assertThat(actualGoalScored, LessThanOrEqual.lessThanOrEqual(4) );
+        assertThat(actualGoalScored, LessThanOrEqual.lessThanOrEqual(2));
+        double originalPI = 3.14;
+        assertThat(originalPI, LessThanOrEqual.lessThanOrEqual(9.00));
+        String authorName = "Sujoy";
+        assertThat(authorName, LessThanOrEqual.lessThanOrEqual("Zachary"));
     }
 }
 
---
+++ src/test/java/com/packtpub/junit/recap/LessThanOrEqual.java
@@ -0,0 +1,33 @@
+package com.packtpub.junit.recap;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Factory;
+
+/**
+ * Created by luis on 30/05/15.
+ */
+public class LessThanOrEqual <T extends Comparable<T>> extends BaseMatcher<Comparable<T>>{
+
+    private final Comparable<T> expectedValue;
+
+    public LessThanOrEqual(T expectedValue) {
+        this.expectedValue = expectedValue;
+    }
+
+   // @Override
+    public void describeTo(Description description) {
+        description.appendText(" less than or equal (<=) expectedValue");
+    }
+   // @Override
+    public boolean matches(Object t) {
+        int compareTo = expectedValue.compareTo((T)t);
+        return compareTo > -1;
+    }
+
+    @Factory
+    public static <T extends Comparable<T>>  Matcher<T> lessThanOrEqual(T t) {
+        return new LessThanOrEqual(t);
+    }
+}
